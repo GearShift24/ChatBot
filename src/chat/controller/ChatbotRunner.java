0c4d002272bb0ba4ff2116059ae7c60b87bb000b
GearShift24 <jwri6847@740s-pl-369.local>
2016-10-14 08:40:51 -0600

    Added a runner

---
+++ src/chat/controller/ChatbotRunner.java
@@ -0,0 +1,10 @@
+package chat.controller;
+
+public class ChatbotRunner
+{
+	public static void main (String [] args)
+	{
+		ChatbotRunner crapChatRunner = new ChatbotRunner();
+		crapChatRunner.start();
+	}
+}
