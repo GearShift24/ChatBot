e090e982d17d550263d46d4a72e24f21e4e0078f
GearShift24 <jwri6847@740s-pl-369.local>
2016-10-18 08:36:34 -0600

    added methods to viewer class

--- src/chat/controller/ChatbotController.java
+++ src/chat/controller/ChatbotController.java
@@ -1,14 +1,17 @@
 package chat.controller;
 
+import chat.model.Chatbot;
 
 public class ChatbotController
 {
+	private ChatbotController stupidBot;
 
+		
 	
-	public void start()
-	{
-		
-		
-	}
-
+		public void start()
+		{
+			
+		}
+	
+	
 }
--- src/chat/view/ChatViewer.java
+++ src/chat/view/ChatViewer.java
@@ -1,6 +1,33 @@
 package chat.view;
 
+import javax.swing.JOptionPane;
+
 public class ChatViewer
 {
 
+	public String collectResponse(String question)
+	{
+		String response = "";
+		
+		response = JOptionPane.showInputDialog(null, question);
+		
+		return response;
+	}
+	
+	
+	public void displayMessage(String message)
+	{
+		
+		JOptionPane.showMessageDialog(null, message);
+		
+	}
+	
+	public int collectUserOptions(String question)
+	{
+		int response = 0;
+		
+		JOptionPane.showConfirmDialog(null, question);
+		
+		return response;
+	}
 }
