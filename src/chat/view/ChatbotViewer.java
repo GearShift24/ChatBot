b3e01188a19b459396fceec16e752b63a82ab783
GearShift24 <jwri6847@740s-pl-369.local>
2016-10-28 07:40:53 -0600

    added icon w/name and changes image get class to the new image

--- src/chat/view/ChatbotViewer.java
+++ src/chat/view/ChatbotViewer.java
@@ -11,7 +11,7 @@ public class ChatbotViewer
 	public ChatbotViewer()
 	{
 		windowMessage = "This message is brought to you by chatbot";
-		chatIcon = new ImageIcon(getClass().getResource("images/chatbot.jpg"));
+		chatIcon = new ImageIcon(getClass().getResource("images/chatbotIcon.jpg"));
 		
 		
 	}
